51407f1c1ec6d5a28ea0ec4d98a0466ad9ef9d5f
Milan Kerslager <milan.kerslager@pslib.cz>
2017-05-04 13:02:32 +0200

    code cleanup, comment

--- app/src/main/java/cz/kerslager/android/bestscore/MainActivity.java
+++ app/src/main/java/cz/kerslager/android/bestscore/MainActivity.java
@@ -5,16 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import java.util.Random;
+
+// Program generuje náhodná čísla a zapamatuje si nejvyšší (do SharedPreferences)
+// Nastavení
 
 public class MainActivity extends AppCompatActivity {
 
-    SharedPreferences pref;
-    final static String TOP_SCORE = "top_score";
-    TextView textViewAktSkore, textViewMaxSkore;
-    Random r;
-    int nejvyssiSkore = 0;
+    private SharedPreferences pref;
+    private final static String TOP_SCORE = "top_score";
+    private TextView textViewAktSkore;
+    private TextView textViewMaxSkore;
+    private Random r;
+    private int nejvyssiSkore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +25,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textViewAktSkore = (TextView) findViewById(R.id.textViewAktSkore);
         textViewMaxSkore = (TextView) findViewById(R.id.textViewMaxSkore);
-        // obnovíme uložené maximální skore z nastavení programu
+        // obnovíme uložené maximální skore z nastavení programu (default je nula)
         pref = getPreferences(MODE_PRIVATE);
         textViewMaxSkore.setText(String.valueOf(pref.getInt(TOP_SCORE, 0)));
         r = new Random();
     }
 
+    // metoda pro uložení nastavení programu do SharedPreferences
     private void ulozNastaveni(String key, int value) {
         SharedPreferences.Editor e = pref.edit();
         e.putInt(key, value);
-        e.commit();
+        e.apply();
     }
 
     public void onKlik(View v) {
